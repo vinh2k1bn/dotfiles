6bcfd935c255b1b9839d23732fc9d4eb310a18bd
Vinh2k1bn <vinh2k1bn@gmail.com>
2022-01-23 17:58:08 +0700

    add startup script

---
+++ .scripts/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
+export QT_QPA_PLATFORM="wayland"
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=sway
+export QT_QPA_PLATFORMTHEME=qt5ct
+exec sway
