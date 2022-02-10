b3a58aea031d6f33534f6d4d8b755a9f9ce0c31c
vinh2k1bn <vinh2k1bn@gmail.com>
2022-02-10 23:53:04 +0700

    add zsh-setup.sh file

---
+++ .scripts/zsh-setup.sh
@@ -0,0 +1,13 @@
+function installOhMyZsh() {
+    local URL='https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh'
+    banner "I will install Oh My Zsh and plugins"
+
+    yes | sudo -u "$SUDO_USER" -- sh -c "$(curl -fsSL $URL)"
+    sudo -u "$SUDO_USER" -- sh -c "
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions"
+}
+
+installOhMyZsh
